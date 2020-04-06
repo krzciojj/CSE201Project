ec00ba40ffff9066dd2617b5e2d77aaf20481c11
azura-cooper <61286815+azura-cooper@users.noreply.github.com>
2020-04-05 20:11:58 -0400

    Update Review.java

--- Review.java
+++ Review.java
@@ -4,13 +4,9 @@ public class Review {
 	int score;
 	String review;
 	User author;
-
-	/**
-	 * Averages all the review scores
-	 * 
-	 * @return The average of all the review scores
-	 */
-	double getAverageScore() {
-		return 0.0;
+	
+	public int getScore() {
+		return score;
 	}
+
 }
