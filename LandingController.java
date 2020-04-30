5741adfc9aafaf76f5cb508d9b2d5e005316c80a
Jak Krutsch <jakkrutsch@gmail.com>
2020-04-30 13:47:26 -0400

    Catalog Implementation

---
+++ LandingController.java
@@ -0,0 +1,22 @@
+package steakstore;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class LandingController {
+	@FXML
+	Button catalogButton;
+	
+	public void openCatalog() throws Exception
+	{
+		Parent catalogLayout = FXMLLoader.load(getClass().getResource("catalog.fxml"));
+		catalogButton.getScene().setRoot(catalogLayout);
+		Scene scene = new Scene(catalogLayout, 1000, 500);
+		Stage window = (Stage) scene.getWindow();
+		window.setScene(scene);
+	}
+}
