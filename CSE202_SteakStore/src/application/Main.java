de06e98e87c8205ce694fd94e46e7e35a1882141
Grant <61118424+cain0x@users.noreply.github.com>
2020-05-03 17:28:25 -0400

    added data file saving and reading

--- CSE202_SteakStore/src/application/Main.java
+++ CSE202_SteakStore/src/application/Main.java
@@ -1,8 +1,18 @@
 package application;
 	
 import javafx.application.Application;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+
 import application.Controller;
 import javafx.stage.Stage;
+import steakstore.Restaurant;
+import steakstore.User;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
@@ -12,12 +22,41 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 
-public class Main extends Application{
+public class Main extends Application implements Serializable{
 	
 	Button button;
-	
-	public static void main(String[] args) {
+	static ArrayList<Restaurant> restaurants = new ArrayList<Restaurant>();
+	public static void main(String[] args) throws ClassNotFoundException, IOException 
+	{
+		createRestaurantData();
+		readRestaurantData();
 		launch(args);
+	}
+	
+	public static void readRestaurantData() throws IOException, ClassNotFoundException
+	{
+		ObjectInputStream objectData = new ObjectInputStream(new FileInputStream("restaurants.dat"));
+		while(objectData.available() != 0)
+		{
+			restaurants.add((Restaurant) objectData.readObject());
+		}
+		objectData.close();
+		System.out.println("test");
+	}
+	
+	public static void createRestaurantData()
+	{
+		try (FileOutputStream fileOut = new FileOutputStream("restaurants.dat");
+	             ObjectOutputStream objectOut = new ObjectOutputStream(fileOut)) {
+				User testuser = new User("name", "pass", "email");
+	            Restaurant test = new Restaurant("asfda", "asfda", "asfda", "asfda", "asfda", testuser);
+	            Restaurant test2 = new Restaurant("aa", "aaa", "aasdfsa", "a1423423a", "a23423a", testuser);
+	            
+	            objectOut.writeObject(test);
+	            objectOut.writeObject(test2);
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
 	}
 	
 	@Override
