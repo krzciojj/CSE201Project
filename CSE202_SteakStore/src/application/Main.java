07a6e4b559ae5c3dd83ea02946fdb14c4ca32c3c
Grant <caingd@miamioh.edu>
2020-05-04 17:52:23 -0400

    Change test restaurants and added default admin user

--- CSE202_SteakStore/src/application/Main.java
+++ CSE202_SteakStore/src/application/Main.java
@@ -80,9 +80,9 @@ public class Main extends Application implements Serializable{
 		try (FileOutputStream fileOut = new FileOutputStream("restaurants.dat");
 	             ObjectOutputStream objectOut = new ObjectOutputStream(fileOut)) {
 				User testuser = new User("name", "pass", "email");
-	            Restaurant test = new Restaurant("asfda", "asfda", "asfda", "asfda", "asfda", "asfda", testuser);
-	            Restaurant test2 = new Restaurant("aa", "aaa", "aasdfsa", "a1423423a", "a1423423a", "a23423a", testuser);
-	            Restaurant test3 = new Restaurant("bb", "a1423423a", "aaa", "aasdfsa", "a1523423a", "a23423a", testuser);
+	            Restaurant test = new Restaurant("Wendy's", "5142 College Corner Pike", "asfda", "asfda", "asfda", "asfda", testuser);
+	            Restaurant test2 = new Restaurant("McDonald's", "1900 University Ave,", "aasdfsa", "a1423423a", "a1423423a", "a23423a", testuser);
+	            Restaurant test3 = new Restaurant("Chipotle", "1 W High St", "aaa", "aasdfsa", "a1523423a", "a23423a", testuser);
 	            
 	            objectOut.writeObject(test);
 	            objectOut.writeObject(test2);
@@ -96,13 +96,9 @@ public class Main extends Application implements Serializable{
 	{
 		try (FileOutputStream fileOut = new FileOutputStream("users.dat");
 	             ObjectOutputStream objectOut = new ObjectOutputStream(fileOut)) {
-				User testuser = new User("name", "pass", "email");
-				User testuser2 = new User("name2", "pass2", "emai2l");
-				User testuser3 = new User("name3", "pass3", "email3");
-	            
-	            objectOut.writeObject(testuser);
-	            objectOut.writeObject(testuser2);
-	            objectOut.writeObject(testuser3);
+				User admin = new User("admin", "abc123", "admin@email.com", true);
+				
+	            objectOut.writeObject(admin);
 	        } catch (IOException e) {
 	            e.printStackTrace();
 	        }
